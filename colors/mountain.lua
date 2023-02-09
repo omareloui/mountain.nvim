e9791337eacd73d95431d08aab527ccd2d6159b3
Omar Eloui <contact@omareloui.com>
2023-02-10 00:50:15 +0200

    feat(recreate): recreate the color scheme
    Take dracula as a base to do it.

---
+++ colors/mountain.lua
@@ -0,0 +1,1 @@
+require("mountain").load()
---
+++ lua/mountain/palette.lua
@@ -0,0 +1,34 @@
+return {
+	foreground = "#f0f0f0",
+	background = "#0f0f0f",
+	black = "#262626",
+	bright_black = "#4c4c4c",
+	red = "#ac8a8c",
+	bright_red = "#c49ea0",
+	green = "#8aac8b",
+	bright_green = "#9ec49f",
+	yellow = "#aca98a",
+	bright_yellow = "#c4c19e",
+	blue = "#8f8aac",
+	bright_blue = "#a39ec4",
+	magenta = "#ac8aac",
+	bright_magenta = "#c49ec4",
+	cyan = "#8aacab",
+	bright_cyan = "#9ec3c4",
+	white = "#e7e7e7",
+	bright_white = "#f0f0f0",
+	pink = "#ac8aac",
+	purple = "#a39ec4",
+	orange = "#c6a679",
+
+	selection = "#191919",
+	comment = "#4c4c4c",
+
+	menu = "#0f0f0f",
+	visual = "#191919",
+
+	gutter_fg = "#393939",
+	nontext = "#4c4c4c",
+
+	none = "NONE",
+}
